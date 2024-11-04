bd48516eec4cc362bb4136d8c45e286d7da1b883
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-04 22:55:58 +0200

    Created Genre, Actor, Cinema, Movie serializers.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,29 @@
-# write serializers here
+from rest_framework import serializers
+from cinema.models import Genre, Actor, CinemaHall, Movie
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name",)
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name",)
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ("name", "rows", "seats_in_row", "capacity")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer()
+    actors = ActorSerializer()
+
+    class Meta:
+        model = Movie
+        fields = ("title", "description", "duration", "genres", "actors")
