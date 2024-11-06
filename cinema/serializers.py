7762fd0089273c1646e9f3dd5dbaa13ca502da50
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-06 14:46:49 +0200

    Added movie_title, cinema_hall_cappacity fields, creted clss Meta in MovieSessionListSerializer.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -58,4 +58,10 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 
 
 class MovieSessionListSerializer(MovieSessionSerializer):
-    movie = serializers.SlugRelatedField(read_only=True, slug_field="title")
+    movie_title = serializers.CharField(source="movie.title", read_only=True)
+    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+    cinema_hall_capacity = serializers.CharField(source="cinema_hall.capacity", read_only=True)
+
+    class Meta:
+        model = MovieSession
+        fields = ("id", "show_time", "movie_title", "cinema_hall_name", "cinema_hall_capacity")
