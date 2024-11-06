d86ac3599297b6adf7b2787efbe2806079be277d
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-06 09:14:56 +0200

    Added 'id' in Meta of MovieSerializer. Created MovieListSerializer, MovieSessionListSerializer. Updated MovieRetrieveSerializer.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -27,18 +27,35 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
 
 class MovieSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Movie
-        fields = ("title", "description", "duration", "genres", "actors")
+        fields = ("id", "title", "description", "duration", "genres", "actors")
 
 
-class MovieSerializerList(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ("id",)
+class MovieListSerializer(MovieSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="full_name"
+    )
+
+
+class MovieRetrieveSerializer(MovieSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
 
 
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
-        fields = ("show_time", "movie", "cinema_hall")
+        fields = ("id", "show_time", "movie", "cinema_hall")
+
+
+class MovieListSessionSerializer(MovieSessionSerializer):
+    movie = serializers.SlugRelatedField(read_only=True, slug_field="title")
